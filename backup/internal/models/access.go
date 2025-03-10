3536b651306a2f3049b493af09e304ddb50da952
ankitprasad2005 <ankitprasad2005@outlook.com>
2025-03-10 10:03:43 +0530

    feat: added data models

--- backup/internal/models/access.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Access struct {
-    AccessID string `json:"access_id" pg:",pk"`
-    Username string `json:"username"`
-    FileID   string `json:"file_id"`
-    Link     string `json:"link"`
-    Expiry   string `json:"expiry"`
-}
--- backup/internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-    Username string `json:"username" pg:",pk"`
-    Name     string `json:"name"`
-    Email    string `json:"email"`
-    Password string `json:"password"`
-}
